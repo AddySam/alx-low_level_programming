c7df6026a1ee2b839c4c3114f2f69e856f772ca8
AddySam <clefaddy@gmail.com>
2023-02-16 20:53:08 -0800

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the printf funtion is to print the comment
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
