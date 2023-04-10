3d2ce7790c0506aedc7eb3b8d9ae6154dbc29427
AddySam <clefaddy@icloud.com>
2023-04-10 09:31:15 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,13 +2,15 @@
 
 /**
  * print_listint - prints all the elements of listint_t
- * at the pointer to the first element of listint
+ * @h: pointer to first element of listint
  *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
+
 {
 	size_t i = 0;
+
 
 	while (h != NULL)
 	{
