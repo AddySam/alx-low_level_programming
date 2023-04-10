3704b9806f7d76a8087189068c540dca2eb6f5ef
AddySam <clefaddy@icloud.com>
2023-04-10 12:28:15 -0700

    Bit manipulation of codes

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned integer
+ * @b: a string containing a binary number
+ * Return: the converted unsigned integer or 0 if an error occurs
+ */
+
+unsigned int binary_to_uint(const char *b)
+
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (; *b != '\0'; ++b) 
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		result = (result << 1) | (*b - '0');
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * flip_bits - Calculates the number of bits that need to be flipped to go from
+ *
+ * one number to another.
+ *
+ * @n: First unsigned long int.
+ * 
+ * @m: Second unsigned long int.
+ *
+ * Return: The number of bits needed to be flipped.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
+{
+	unsigned long int diff = n ^ m;
+	unsigned int count = 0;
+
+	while (diff > 0)
+	{
+		if (diff & 1)
+			count++;
+		diff >>= 1;
+	}
+
+	return (count);
+}
